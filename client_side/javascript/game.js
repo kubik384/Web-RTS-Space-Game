a68b16570dfe1bb5e4fffce1be99c15afd78d267
Jan Kubík <jan.kubik@sde.cz>
2020-06-27 22:21:00 +0200

    Functional authorization system
    Added functional authorization system with redirect to login page when trying to access game page. Mostly garbage
     + insecure code, but it works.

--- client_side/javascript/game.js
+++ client_side/javascript/game.js
@@ -3,15 +3,10 @@
 import { Game } from './modules/game.js';
 
 var game;
-var preventInput = false;
-
 var socket = io();
 
-function request_data() {
-	socket.emit('new_player');
-}
-
 function start() {
+	document.removeEventListener('DOMContentLoaded', start);
 	game = new Game(socket);
 
 	socket.on('Message', message => {
@@ -23,22 +18,11 @@ function start() {
 	});
 
 	game.process_incoming_message('Connecting to the server...', false);
-	request_data();
-
-
-	
-	document.addEventListener("keydown", e => {
-		game.process_keyDown_input(e);
-	});
-
-	document.addEventListener("keyup", e => {
-		game.process_keyUp_input(e);
-	});
-	
-
-	document.addEventListener("paste", e => {
-		game.process_paste_input(e);
-	});
+	game.request_data();
 }
 
-document.addEventListener("DOMContentLoaded", start);
+if (document.readyState !== 'loading') {
+	start();
+} else {
+	document.addEventListener("DOMContentLoaded", start);
+}
--- server_main.js
+++ server_main.js
@@ -2,6 +2,7 @@
 
 const saltRounds = 10;
 const gameURL = '/game';
+var players = {};
 
 var express = require('express');
 var app = express();
@@ -10,6 +11,7 @@ var http = require('http');
 var path = require('path');
 var server = http.Server(app);
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
 var bcrypt = require('bcrypt');
 var io = require('socket.io')(server, {pingInterval: 1500});
 
@@ -28,13 +30,15 @@ con.connect( err => { if (err) throw err; });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(cookieParser());
+
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/client_side', 'pages/index.html'));
 });
 
 app.post('/register', function(req, res) {
-	var username = req.body.username;
-	var password = req.body.password;
+	var { username, password } = req.body;
+	
 	if (password != '' && username != '') {
 		var sql = "SELECT password FROM players WHERE username = '" + username + "'";
 		con.query(sql, function (err, results) {
@@ -62,8 +66,7 @@ app.post('/register', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-	var username = req.body.username;
-	var password = req.body.password;
+	var { username, password } = req.body;
 	
 
 	var sql = "SELECT password FROM players WHERE username = '" + username + "'";
@@ -77,19 +80,32 @@ app.post('/login', function(req, res) {
 					throw err;
 				}
 				if (passwordsMatch) {
-					//authorize the client
+					//Client saves username as token, which is then sent from client to the server to authorize actions sent through socket for every action. If players object does not have attribute equal to token, then action is not executed and user is redirected back to login page instead
+					players[username] = true;
+					res.cookie('token', username, { maxAge: 900000, httpOnly: true })
+					//Would use redirect, however according to answers from stack overflow, when using ajax, express redirect does not work and has to be created from client's side instead
 					res.send(req.protocol + '://' + req.get('host') + gameURL);
 				} else {
 					res.sendStatus(401);
 				}
 			});
+		} else {
+			res.sendStatus(401);
 		}
 	});
 });
 
 app.get(gameURL, function(req,res) {
-	//if authorized, send: 
-	res.sendFile(path.join(__dirname + '/client_side', 'pages/game.html'));
+	if (req.cookies.token !== undefined) {
+		if (players[req.cookies.token]) {
+			res.sendFile(path.join(__dirname + '/client_side', 'pages/game.html'));
+		} else {
+			res.clearCookie('token');
+		}
+	} else {
+		res.redirect(303, '/');
+	}
+	
 });
 
 // Starts the server
@@ -99,18 +115,18 @@ server.listen(8080, function() {
 
 // Add the WebSocket handlers
 io.on('connection', socket => {
-	socket.on('new_player', () => {
-		socket.emit('Message', 'Connection established...');
-		game.add_player(socket.id);
-		socket.emit('Message', 'Number of players: ' + game.get_player_number());
-	}); 
+	socket.on('login_player', token => {
+		socket.emit('Message', 'Connection established, number of players: ' + game.get_player_number());
+		//game.add_player(socket.id);
+	});
 
-	socket.on('command', command => {
+	socket.on('command', (command, token) => {
 		socket.emit('Message', game.process_command(command), socket.id);
 	});
 
-	socket.on('disconnect', () => {
+	socket.on('disconnect', token => {
 		game.remove_player(socket.id);
+		delete players[token];
 	});
 });
 
