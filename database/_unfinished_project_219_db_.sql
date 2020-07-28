d0928da80b7933c82d11cf50d3f69a38978eea96
Jan Kubík <jan.kubik@sde.cz>
2020-07-28 08:51:56 +0200

    Removed database, added database export

---
+++ database/_unfinished_project_219_db_.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Jul 28, 2020 at 06:51 AM
+-- Server version: 8.0.18
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `improvisationaldb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+CREATE TABLE IF NOT EXISTS `players` (
+  `player_id` mediumint(9) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `wood` float NOT NULL DEFAULT '100',
+  `wood_prod` float NOT NULL DEFAULT '0.0028',
+  `dirt` float NOT NULL DEFAULT '100',
+  `dirt_prod` float NOT NULL DEFAULT '0.0028',
+  `iron` float NOT NULL DEFAULT '100',
+  `iron_prod` float NOT NULL DEFAULT '0.0028',
+  `pop` float NOT NULL DEFAULT '100',
+  `pop_prod` float NOT NULL DEFAULT '0.0028',
+  `res_gen_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`player_id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_buildings`
+--
+
+DROP TABLE IF EXISTS `player_buildings`;
+CREATE TABLE IF NOT EXISTS `player_buildings` (
+  `player_id` mediumint(8) UNSIGNED NOT NULL,
+  `building_id` smallint(5) UNSIGNED NOT NULL,
+  `level` smallint(4) UNSIGNED NOT NULL,
+  `upgradeStart` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`building_id`,`player_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
