61eee3191ceb0b2bda8795dc909ae8edea27e7a5
John Borlen <jan.kubik@sde.cz>
2023-11-11 19:46:20 +0100

    Moved files into page folders

---
+++ client_side/pages/report_page/report_init.js
@@ -0,0 +1,26 @@
+"use strict";
+
+import { Game } from './report.js';
+
+var socket = io({ auth: { token: document.cookie.split('token=')[1] }} );
+var game = new Game(socket);
+game.request_datapack();
+
+socket.on('report_datapack', game.save_reports.bind(game));
+
+async function start() {
+	game.display_reports();
+	socket.on('report_details', game.load_report.bind(game));
+	socket.on('new_report', game.add_new_report_counter);
+	socket.on('fr_availability', game.get_fr_status.bind(game));
+	document.removeEventListener('DOMContentLoaded', start);
+}
+
+
+
+
+if (document.readyState !== 'loading') {
+	start();
+} else {
+	document.addEventListener("DOMContentLoaded", start);
+}
